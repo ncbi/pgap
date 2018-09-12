f6466cedb109680870a7dd37745bdeea78a4fb46
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2018-09-12 07:02:00 -0400

    Updated remote supplemental data file name, NOJIRA

--- scripts/fetch_supplemental_data.sh
+++ scripts/fetch_supplemental_data.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 VERSION=`grep dockerPull wf_pgap.cwl | cut -d: -f3`
-wget -qO- https://s3.amazonaws.com/pgap-data/$VERSION.tgz | tar xvz
+wget -qO- https://s3.amazonaws.com/pgap-data/input-${VERSION}.tgz | tar xvz
