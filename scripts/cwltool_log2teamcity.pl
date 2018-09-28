54caf0fac5124361a4fa73df6b2f30ea52c3c5d1
Wratko Hlavina <whlavina@ncbi.nlm.nih.gov>
2018-09-28 09:11:19 -0400

    Improve cwltool to TeamCity log conversion. JIRA: PGAPX-188.

--- scripts/cwltool_log2teamcity.pl
+++ scripts/cwltool_log2teamcity.pl
@@ -8,38 +8,55 @@ use warnings;
 
 {
     my $stack = [];
+    my %started = (); # Count of times a step was started.
     my $header_printed = 0;
+    my $test_suite;
     while(<>) {
         chomp;
+        if (/Resolved '([^']+)'/ and not defined $test_suite) {
+            $test_suite = $1;
+        }
         my $k={}; 
-        ($$k{curtime}, $$k{step}, $$k{message}) = m{\[([^\]]+)\] \[([^\]]+)\] (.*)}g;
+        ($$k{curtime}, $$k{step}, $$k{message}) = /\[(\d+-\d+-\d+ \d+:\d+:\d+)\] \[((?:workflow|step|job) [^\]]+)\] (.*)/;
         if (!defined $$k{message}) {
 			print "$_\n";
 			next;
 		}
-        if ($$k{message} =~ /^completed/) { # new step
+        $$k{step} =~ s/\s+$//; # Remove trailing space in unnamed "[workflow ]".
+        if ($$k{message} =~ /^completed/) { # Completed (success/permanentFail) what was started.
             # my $kl = $$stack[scalar(@$stack)-1];
+            --$started{$$k{step}};
 			print "$_\n";
 			if( $$k{message} =~/permanentFail/ ) {
 				&tc_failed($$k{step}, $$k{message});
 			}
-			else {
-				&tc_finish($$k{step});
-			}
+            &tc_finish($$k{step});
             pop @$stack;
         }
-        else {
-			&tc_start($$k{step});
-			print "$_\n";
+        elsif ($$k{message} =~ /^start$/ or not defined $started{$$k{step}})  { # Starting new step/job.
+            ++$started{$$k{step}};
+            if (not $header_printed) {
+                $header_printed = 1;
+                $test_suite = 'cwltool' unless defined $test_suite;
+                &tc_start_suite($test_suite);
+            }
+            &tc_start($$k{step});
+            print "$_\n";
             push @$stack, $k;
         }
+        else {
+            print "$_\n";
+        }
+    }
+    if ($header_printed) {
+        tc_finish_suite($test_suite);
     }
 } exit 0;
 
 sub tc_start
 {
 	my $name = shift;
-    printf '%s%s%s', '#', '#', "teamcity[testStarted name='$name' captureStandardOutput='true']";
+    printf '%s%s%s', '#', '#', "teamcity[testStarted flowId='$name' name='$name' captureStandardOutput='true']";
 	print "\n";
 }
 
@@ -53,7 +70,7 @@ sub tc_start_suite
 sub tc_finish
 {
 	my $name = shift;
-    printf '%s%s%s', '#', '#', "teamcity[testFinished name='$name']";
+    printf '%s%s%s', '#', '#', "teamcity[testFinished flowId='$name' name='$name']";
 	print "\n";
 }
 
@@ -68,6 +85,6 @@ sub tc_failed
 {
 	my ($name, $message) = @_;
 	chomp $message;
-    printf '%s%s%s', '#', '#', "teamcity[testFailed name='$name' message='$message']";
+    printf '%s%s%s', '#', '#', "teamcity[testFailed flowId='$name' name='$name' message='$message']";
 	print "\n";
 }
