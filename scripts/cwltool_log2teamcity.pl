3af5819a51f339243317df3479746f2165a44831
Wratko Hlavina <whlavina@ncbi.nlm.nih.gov>
2018-10-10 12:43:07 -0400

    Use nested blocks for CWL hierarchy (assumes non-interleaved execution). JIRA: PGAPX-188.

--- scripts/cwltool_log2teamcity.pl
+++ scripts/cwltool_log2teamcity.pl
@@ -11,36 +11,40 @@ use warnings;
     my %started = (); # Count of times a step was started.
     my $header_printed = 0;
     my $test_suite;
+    my $key;
     while(<>) {
         chomp;
         if (/Resolved '([^']+)'/ and not defined $test_suite) {
             $test_suite = $1;
         }
         my $k={}; 
-        ($$k{curtime}, $$k{step}, $$k{message}) = /\[(\d+-\d+-\d+ \d+:\d+:\d+)\] \[((?:workflow|step|job) [^\]]+)\] (.*)/;
+        ($$k{curtime}, $$k{type}, $$k{step}, $$k{message}) = /\[(\d+-\d+-\d+ \d+:\d+:\d+)\] \[(workflow|step|job) ([^\]]+)\] (.*)/;
         if (!defined $$k{message}) {
 			print "$_\n";
 			next;
 		}
-        $$k{step} =~ s/\s+$//; # Remove trailing space in unnamed "[workflow ]".
+        $key = $$k{type};
+        $key .= " $$k{step}" if $$k{step};
         if ($$k{message} =~ /^completed/) { # Completed (success/permanentFail) what was started.
             # my $kl = $$stack[scalar(@$stack)-1];
-            --$started{$$k{step}};
+            --$started{$key};
 			print "$_\n";
 			if( $$k{message} =~/permanentFail/ ) {
-				&tc_failed($$k{step}, $$k{message});
+				&tc_failed($key, $$k{message});
 			}
-            &tc_finish($$k{step});
+            &tc_finish($key);
+            &tc_block_closed($key) unless $$k{type} eq "job";
             pop @$stack;
         }
-        elsif ($$k{message} =~ /^start$/ or not defined $started{$$k{step}})  { # Starting new step/job.
-            ++$started{$$k{step}};
+        elsif ($$k{message} =~ /^start$/ or not defined $started{$key})  { # Starting new step/job.
+            ++$started{$key};
             if (not $header_printed) {
                 $header_printed = 1;
                 $test_suite = 'cwltool' unless defined $test_suite;
                 &tc_start_suite($test_suite);
             }
-            &tc_start($$k{step});
+            &tc_block_opened($key, $$k{step}) unless $$k{type} eq "job";
+            &tc_start($key);
             print "$_\n";
             push @$stack, $k;
         }
@@ -88,3 +92,19 @@ sub tc_failed
     printf '%s%s%s', '#', '#', "teamcity[testFailed flowId='$name' name='$name' message='$message']";
 	print "\n";
 }
+
+sub tc_block_opened
+{
+    my ($name, $description) = @_;
+    chomp $description;
+    printf '%s%s%s', '#', '#', "teamcity[blockOpened name='$name' description='$description']";
+    print "\n";
+}
+
+sub tc_block_closed
+{
+    my ($name) = @_;
+    printf '%s%s%s', '#', '#', "teamcity[blockClosed name='$name']";
+    print "\n";
+}
+
