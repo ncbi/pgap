a8180ee5544196a3bdfd1ce09f55baa4ff86dbc3
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2018-11-08 13:16:50 -0500

    Created scripts to run the pipeline within a single docker container

---
+++ scripts/run_pgap_standalone.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+set -euxo pipefail
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ${0} <input yaml>"
+    exit
+fi
+
+VERSION=`cat VERSION`
+image="ncbi/pgap:${VERSION}"
+inDir=`cd $(dirname $1) && pwd -P`
+inFile=`basename $1`
+
+
+# Create the input file
+cat $1 <(docker run -i ${image} cat /pgap/input.yaml) > $inDir/pgap_input.yaml
+
+# Ensure needed files are setup
+mkdir -p $inDir/input
+mkdir -p output
+ln -s /pgap/input_template $inDir/input_template
+
+# Run the actual workflow
+docker run -i \
+       --volume $inDir:/pgap/user_input:ro \
+       --volume $(pwd -P)/input-${VERSION}:/pgap/user_input/input:ro \
+       --volume $(pwd -P)/output:/pgap/output:rw \
+       ncbi/pgap:2018-11-07.build3190 \
+       cwltool \
+       --outdir ./outdir \
+       wf_pgap_simple.cwl \
+       user_input/pgap_input.yaml
+
+# Cleanup
+rmdir $inDir/input
+rm $inDir/input_template
---
+++ scripts/setup_pgap_standalone.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+set -euo pipefail
+
+curl --silent "https://api.github.com/repos/ncbi/pgap/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' > VERSION
+VERSION=`cat VERSION`
+#wget -nc https://s3.amazonaws.com/pgap-data/input-${VERSION}.tgz
+#echo -n "Unpacking test_genomes..."
+#tar xzf input-${VERSION}.tgz
+#echo "done!"
+
+wget -nc https://s3.amazonaws.com/pgap-data/test_genomes.tgz
+echo -n "Unpacking test_genomes..."
+tar xzf test_genomes.tgz
+echo "done!"
+
+docker pull ncbi/pgap:${VERSION}
