d47c2a0af0476b87be03ba0941be766012a49580
Douglas Slotta <slottad@ncbi.nlm.nih.gov>
2018-11-08 13:43:32 -0500

    Fixed input data download, added run_pgap_standalone.sh

--- scripts/setup_pgap_standalone.sh
+++ scripts/setup_pgap_standalone.sh
@@ -3,10 +3,13 @@ set -euo pipefail
 
 curl --silent "https://api.github.com/repos/ncbi/pgap/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' > VERSION
 VERSION=`cat VERSION`
-#wget -nc https://s3.amazonaws.com/pgap-data/input-${VERSION}.tgz
-#echo -n "Unpacking test_genomes..."
-#tar xzf input-${VERSION}.tgz
-#echo "done!"
+
+wget https://github.com/ncbi/pgap/raw/master/scripts/run_pgap_standalone.sh
+
+wget -nc https://s3.amazonaws.com/pgap-data/input-${VERSION}.tgz
+echo -n "Unpacking test_genomes..."
+tar xzf input-${VERSION}.tgz
+echo "done!"
 
 wget -nc https://s3.amazonaws.com/pgap-data/test_genomes.tgz
 echo -n "Unpacking test_genomes..."
